c1149c4ef617b3fc3995bfdcea20d15dfa7f617e
namickey <teethdai@gmail.com>
2023-02-25 21:37:10 +0900

    add subnet

--- main.tf
+++ main.tf
@@ -7,10 +7,28 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_vpc" "test-vpc" {
+resource "aws_vpc" "orange" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "test2-vpc"
+    Name = "orange-vpc"
+  }
+}
+
+resource "aws_subnet" "public_1a" {
+  vpc_id = "${aws_vpc.orange.id}"
+  availability_zone = "ap-northeast-1a"
+  cidr_block        = "10.0.1.0/24"
+  tags = {
+    Name = "orange-subpub-1a"
+  }
+}
+
+resource "aws_subnet" "public_1c" {
+  vpc_id = "${aws_vpc.orange.id}"
+  availability_zone = "ap-northeast-1c"
+  cidr_block        = "10.0.2.0/24"
+  tags = {
+    Name = "orange-subpub-1c"
   }
 }
 
