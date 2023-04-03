f8e0edf4d53ad71b06591afa4b35fe6b061ebfc6
namickey <teethdai@gmail.com>
2023-04-04 00:15:28 +0900

    fix userdata

--- setup.sh
+++ setup.sh
@@ -2,15 +2,17 @@
 
 echo hello
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+sudo -u ec2-user cd
 
-. ~/.nvm/nvm.sh
+sudo -u ec2-user curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
-nvm install 16
+sudo -u ec2-user . ~/.nvm/nvm.sh
 
-yum install git
+sudo -u ec2-user nvm install 16
 
-git clone https://github.com/namickey/nuxt-try.git
+sudo -u ec2-user yum install git
+
+sudo -u ec2-user git clone https://github.com/namickey/nuxt-try.git
 
 
 
