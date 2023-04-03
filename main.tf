088ae24953b7f5e590e4d10e53a28465a4cf6917
namickey <teethdai@gmail.com>
2023-04-03 23:56:29 +0900

    fix

--- main.tf
+++ main.tf
@@ -112,7 +112,7 @@ resource "aws_security_group_rule" "inbound_ssh" {
   security_group_id = aws_security_group.orange-sg.id
 }
 
-resource "aws_security_group_rule" "inbound_ssh" {
+resource "aws_security_group_rule" "inbound_nodejs" {
   type      = "ingress"
   from_port = 3000
   to_port   = 3000
