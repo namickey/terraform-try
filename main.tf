1921835a83fbb26182d06e4df15e0530a76c975d
namickey <teethdai@gmail.com>
2023-04-03 22:40:28 +0900

    fix key

--- main.tf
+++ main.tf
@@ -127,7 +127,7 @@ resource "aws_instance" "orange" {
   ami                     = "ami-0ffac3e16de16665e"
   instance_type           = "t2.micro"
   disable_api_termination = false
-  key_name                = "orange"
+  key_name                = "nuxt3"
   vpc_security_group_ids  = [aws_security_group.orange-sg.id]
   subnet_id               = aws_subnet.pub-1a.id
   associate_public_ip_address = "true"
