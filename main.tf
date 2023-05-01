21883e00610fc7bc59915660e20fb1dddd4ffa6c
namickey <teethdai@gmail.com>
2023-05-01 22:24:36 +0900

    add flask

--- main.tf
+++ main.tf
@@ -134,6 +134,17 @@ resource "aws_security_group_rule" "inbound_http" {
   security_group_id = aws_security_group.orange-sg.id
 }
 
+resource "aws_security_group_rule" "inbound_flask" {
+  type      = "ingress"
+  from_port = 5000
+  to_port   = 5000
+  protocol  = "tcp"
+  cidr_blocks = [
+    "0.0.0.0/0",
+  ]
+  security_group_id = aws_security_group.orange-sg.id
+}
+
 resource "aws_security_group_rule" "outbound_all" {
   type      = "egress"
   from_port = 0
