5bb35540ec3452c88ea19bda3a10584879fa148c
namickey <teethdai@gmail.com>
2023-05-01 22:00:03 +0900

    add http port

--- main.tf
+++ main.tf
@@ -123,6 +123,17 @@ resource "aws_security_group_rule" "inbound_nodejs" {
   security_group_id = aws_security_group.orange-sg.id
 }
 
+resource "aws_security_group_rule" "inbound_http" {
+  type      = "ingress"
+  from_port = 80
+  to_port   = 80
+  protocol  = "tcp"
+  cidr_blocks = [
+    "0.0.0.0/0",
+  ]
+  security_group_id = aws_security_group.orange-sg.id
+}
+
 resource "aws_security_group_rule" "outbound_all" {
   type      = "egress"
   from_port = 0
